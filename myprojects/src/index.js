3d58b93fe3368cc657db1ef10ac99f173691a9f5
Graciliano-Barroso <grico484@gmail.com>
2022-12-20 15:59:36 -0400

    feat: Exclusão de um projeto

--- myprojects/src/index.js
+++ myprojects/src/index.js
@@ -48,11 +48,18 @@ app.put('/projects/:id', (request, response) => {
     return response.json(project);
 });
 
-app.delete('/projects/:id', (require, response) => {
-    return response.json([
-        'Projeto 2',
-        'Projeto 3'
-    ]);
+app.delete('/projects/:id', (request, response) => {
+    const {id} = request.params;
+
+    const projectIndex = projects.findIndex(p => p.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(404).json({ error: 'Project not found' });
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
 });
 
 app.listen(3000, () => {
