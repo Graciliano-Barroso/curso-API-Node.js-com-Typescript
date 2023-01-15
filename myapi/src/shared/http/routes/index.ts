ce94fd8e82cc1c9155bd9cac4d86eae09d0942a2
Graciliano-Barroso <grico484@gmail.com>
2023-01-14 20:55:02 -0400

    feat: Adicionado Errors Middleware

--- myapi/src/shared/http/routes/index.ts
+++ myapi/src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
+import { AppError } from "@shared/errors/AppError";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.get("/", (request, response) => {
-  return response.json({ message: "Olá Dev!!!s" });
+  throw new AppError("Acesso negado", 401);
+  return response.json({ message: "Olá Dev!" });
 });
 
-export {routes}
+export { routes }
