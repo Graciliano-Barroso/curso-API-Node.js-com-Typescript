56e51f6011531558fc068b5ab58bb4808cf36db6
Graciliano-Barroso <grico484@gmail.com>
2023-02-12 17:41:04 -0400

    feat: Creating a dependency injection container and refactor createRole

--- myapi/src/roles/useCases/createRole/CreateRoleController.ts
+++ myapi/src/roles/useCases/createRole/CreateRoleController.ts
@@ -1,12 +1,12 @@
 import { CreateRoleUseCase } from "./CreateRoleUseCase";
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 export class CreateRoleController {
-  constructor(private createRoleUseCase: CreateRoleUseCase) {}
-
   async handle(request: Request, response: Response): Promise<Response> {
+    const createRoleUseCase = container.resolve(CreateRoleUseCase);
     const { name } = request.body;
-    const role = await this.createRoleUseCase.execute({ name });
+    const role = await createRoleUseCase.execute({ name });
 
     return response.status(201).json(role);
   }
