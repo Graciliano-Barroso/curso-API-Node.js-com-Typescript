d262e29c6b3d90514905d02a1eca117a29cb2713
Graciliano-Barroso <grico484@gmail.com>
2023-02-12 18:12:50 -0400

    refactor: DeleteRole

--- myapi/src/roles/useCases/deleteRole/DeleteRoleController.ts
+++ myapi/src/roles/useCases/deleteRole/DeleteRoleController.ts
@@ -1,12 +1,12 @@
+import { container } from "tsyringe";
 import { DeleteRoleUseCase } from "./DeleteRoleUseCase";
 import { Request, Response } from "express";
 
 export class DeleteRoleController {
-  constructor(private deleteRoleUseCase: DeleteRoleUseCase) {}
-
   async handle(request: Request, response: Response): Promise<Response> {
+    const deleteRoleUseCase = container.resolve(DeleteRoleUseCase);
     const { id } = request.params;
-    await this.deleteRoleUseCase.execute({ id });
+    await deleteRoleUseCase.execute({ id });
 
     return response.status(204).send();
   }
