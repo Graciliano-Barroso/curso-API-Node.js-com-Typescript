4cc3a29942909aae24284752241abb84577d823e
Graciliano-Barroso <grico484@gmail.com>
2023-02-12 19:15:41 -0400

    refactor: UpdateRole

--- myapi/src/roles/useCases/updateRole/index.ts
+++
@@ -1,9 +0,0 @@
-import { RolesRepository } from "@roles/repositories/RolesRepository";
-import { UpdateRoleUseCase } from "./UpadateRoleUseCase";
-import { UpadateRoleController } from "./UpdateRoleController";
-
-const rolesRepository = RolesRepository.getInstance();
-const updateRoleUseCase = new UpdateRoleUseCase(rolesRepository);
-export const upadateRolesController = new UpadateRoleController(
-  updateRoleUseCase,
-);
