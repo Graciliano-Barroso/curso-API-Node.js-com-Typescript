794935dffa76917d8827a0278916a6253a0a55c4
Graciliano-Barroso <grico484@gmail.com>
2023-02-13 11:56:07 -0400

    feat: Create migration AddRoleIdToUsersTable

--- myapi/src/shared/typeorm/index.ts
+++ myapi/src/shared/typeorm/index.ts
@@ -2,10 +2,15 @@ import { DataSource } from "typeorm";
 import { Role } from "@roles/entities/Role";
 import { CreateRolesTable1675090192981 } from "./migrations/1675090192981-CreateRolesTable";
 import { CreateUsersTable1676299947201 } from "./migrations/1676299947201-CreateUsersTable";
+import { AddRoleIdToUsersTable1676302141631 } from "./migrations/1676302141631-AddRoleIdToUsersTable";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
   entities: [Role],
-  migrations: [CreateRolesTable1675090192981, CreateUsersTable1676299947201],
+  migrations: [
+    CreateRolesTable1675090192981,
+    CreateUsersTable1676299947201,
+    AddRoleIdToUsersTable1676302141631,
+  ],
 });
