153afb64a147bd339e20f4c2017f2952941ed7db
Graciliano-Barroso <grico484@gmail.com>
2023-02-14 09:46:21 -0400

    feat: Create user entity

--- myapi/src/shared/typeorm/index.ts
+++ myapi/src/shared/typeorm/index.ts
@@ -3,11 +3,12 @@ import { Role } from "@roles/entities/Role";
 import { CreateRolesTable1675090192981 } from "./migrations/1675090192981-CreateRolesTable";
 import { CreateUsersTable1676299947201 } from "./migrations/1676299947201-CreateUsersTable";
 import { AddRoleIdToUsersTable1676302141631 } from "./migrations/1676302141631-AddRoleIdToUsersTable";
+import { User } from "@users/entities/User";
 
 export const dataSource = new DataSource({
   type: "sqlite",
   database: "./db.sqlite",
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1675090192981,
     CreateUsersTable1676299947201,
