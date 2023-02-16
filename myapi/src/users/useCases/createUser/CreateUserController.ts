aa099a190af5b52e0665b4bac97802ba37132f8c
Graciliano-Barroso <grico484@gmail.com>
2023-02-16 14:20:11 -0400

    feat: Add CreateUserController

---
+++ myapi/src/users/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,18 @@
+import { container } from "tsyringe";
+import { Request, Response } from "express";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+export class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+    const { name, email, password, isAdmin, roleId } = request.body;
+    const user = await createUserUseCase.execute({
+      name,
+      email,
+      password,
+      isAdmin,
+      roleId,
+    });
+    return response.status(201).json(user);
+  }
+}
