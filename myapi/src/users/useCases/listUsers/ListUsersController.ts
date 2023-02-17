269926a52c825f46e11e983a7c72a8c3961efbab
Graciliano-Barroso <grico484@gmail.com>
2023-02-17 12:57:25 -0400

    feat: Create use case and controller for user listing

--- myapi/src/users/useCases/listUsers/ListUsersController.ts
+++ myapi/src/users/useCases/listUsers/ListUsersController.ts
@@ -1,10 +1,10 @@
-import { container } from "tsyringe";
 import { Request, Response } from "express";
-import { ListRolesUseCase } from "./ListRolesUseCase";
+import { container } from "tsyringe";
+import { ListUsersUseCase } from "./ListUsersUseCase";
 
 export class ListUsersController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const listRolesUseCase = container.resolve(ListRolesUseCase);
+    const listUsersUseCase = container.resolve(ListUsersUseCase);
     const page =
       request.query.page && Number(request.query.page) > 0
         ? Number(request.query.page)
@@ -13,7 +13,7 @@ export class ListUsersController {
       request.query.limit && Number(request.query.limit) > 0
         ? Number(request.query.limit)
         : 15;
-    const roles = await listRolesUseCase.execute({ page, limit });
-    return response.json(roles);
+    const users = await listUsersUseCase.execute({ page, limit });
+    return response.json(users);
   }
 }
