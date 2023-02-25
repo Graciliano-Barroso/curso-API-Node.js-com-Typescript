048a3ea83afa49e53a6626cf30b6d50c6f18a338
Graciliano-Barroso <grico484@gmail.com>
2023-02-25 05:54:25 -0400

    feat: RefreshToken repository

---
+++ myapi/src/users/repositories/RefreshTokenRepository.ts
@@ -0,0 +1,45 @@
+import { RefreshToken } from './../entities/RefreshToken';
+import { dataSource } from "@shared/typeorm";
+import { RefreshToken } from "@users/entities/RefreshToken";
+import { Repository } from "typeorm";
+import {
+  CreateRefreshTokenDTO,
+  IRefreshTokenRepository,
+} from "./IRefreshTokenRepository";
+import { AppError } from '@shared/errors/AppError';
+
+export class RefreshTokenRepository implements IRefreshTokenRepository {
+  private repository: Repository<RefreshToken>;
+
+  constructor() {
+    this.repository = dataSource.getRepository(RefreshToken);
+  }
+
+  async create({
+    user_id,
+    token,
+    expires,
+    valid,
+  }: CreateRefreshTokenDTO): Promise<RefreshToken> {
+    const refreshToken = this.repository.create({
+      user_id,
+      token,
+      expires,
+      valid,
+    });
+    return this.repository.save(refreshToken);
+  }
+
+  async findByToken(token: string): Promise<RefreshToken> {
+    return this.repository.findOneBy({ token });
+  }
+
+  async invalidate(refresh_token: RefreshToken): Promise<void> {
+    const refreshToken = await this.findByToken(refresh_token.token);
+    if (!refreshToken) {
+      throw new AppError("Refresh Token not found");
+    }
+    refreshToken.valid = false;
+    await this.repository.save(refreshToken);
+  }
+}
